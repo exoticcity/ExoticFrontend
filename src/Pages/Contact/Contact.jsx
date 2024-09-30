94950efd3af524966d7d92199c44860b4b87b7a7
haseebinvader <rhaseeb741@gmail.com>
2024-09-30 12:57:40 +0500

    Contact Form Fields Updated

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,25 +3,25 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
 import TermsConditions from "./TermsConditions";
 
 const Contact = () => {
   const { t } = useTranslation();
-  const [isChecked, setisChecked] = useState(false)
+  const [isChecked, setisChecked] = useState(false);
 
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs.sendForm("service_1g7imx4", "template_8uek6dd", form.current, "KgNk2-GMoPQOnxjqX")
       .then((res) => {
-        toast.success("Message Sent")
+        toast.success("Message Sent");
         console.log(res);
         form.current.reset();
         setisChecked(false);
       },
         (error) => {
-          toast.error("Server is down please try again after a few minutes");
+          toast.error("Server is down, please try again after a few minutes");
           console.log(error);
         });
   };
@@ -38,11 +38,14 @@ const Contact = () => {
         </Box>
       </Grid>
       <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box sx={{ width: '90%' }}>
+        <Box sx={{ width: '90%', padding: '30px', borderRadius: '20px' }}>
           <form ref={form} onSubmit={sendEmail}>
             <Box sx={{ mt: { xs: '1rem', sm: 0 }, display: 'flex', flexDirection: 'column' }}>
-              <TextField fullWidth label="Name" size="small" type="text" name="user_email" required />
-              <TextField sx={{ mt: '1rem' }} fullWidth label="Email" size="small" name="user_name" required />
+              <TextField fullWidth label="Name" size="small" type="text" name="user_name" required />
+              <TextField sx={{ mt: '1rem' }} fullWidth label="Company Name" size="small" name="company_name" />
+              <TextField sx={{ mt: '1rem' }} fullWidth label="Email" size="small" name="user_email" required />
+              <TextField sx={{ mt: '1rem' }} fullWidth label="WhatsApp Number" size="small" name="whatsapp_number" />
+              <TextField sx={{ mt: '1rem' }} fullWidth label="Address" size="small" name="address" />
               <TextField sx={{ mt: '1rem' }} fullWidth label="Message" multiline rows={4} name="message" required />
               <Box sx={{ mt: '1rem', display: 'flex', justifyContent: 'space-between' }}>
                 <TermsConditions />
